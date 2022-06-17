6c312ba76b0478cec719727669533acfc680106c
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-17 12:47:20 +0300

    commit tree f9f24eee1854f2738ef01a99aaf1f274b0c302a8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlock){
+    if(distanceInBlock >42){
+        return distanceInBlock - 42
+    }
+    else{
+        return 42-distanceInBlock
+    }
+ }
+ console.log(distanceFromHqInBlocks(50))
+ function distanceFromHqInFeet (distanceInBlock) {
+     return distanceFromHqInBlocks(distanceInBlock) * 264;
+   }
+   
+ function distanceTravelledInFeet(start, destination){
+     if(start<destination){
+        return(destination-start) * 264
+     }else{
+        return(start-destination) * 264
+    }
+ }
+ console.log(distanceTravelledInFee(43, 45))
+ function calculatesFarePrice(start, destination){
+     const distance = distanceTravelledInFeet(start, destination)
+     if(distance <= 400){
+         return 0
+     }
+     else if(distance >=400 && distance<= 2000){
+         return 0.02 * (distance-400);
+     }
+     else if(distance > 2000 && distance <= 2500){
+         return 25;
+     }else{
+         return 'cannot travel that far'
+     }
+ }
+ 
